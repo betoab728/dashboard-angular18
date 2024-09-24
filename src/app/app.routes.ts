2a9ce95b88e02af0f1506154474f4bd3b6ee5e13
betoab728 <betoab728@gmail.com>
2024-09-24 18:58:35 -0500

    modelo de dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        loadComponent: () => import('./components/layout/layout.component'),
+        children: [
+            {
+                path: 'dashboard',
+                loadComponent: () => import('./pages/dashboard/dashboard.component')
+            },
+            {
+                path: 'profile',
+                loadComponent: () => import('./pages/profile/profile.component')
+            },
+            {
+                path: 'tables',
+                loadComponent: () => import('./pages/tables/tables.component')
+            },
+            {
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full'
+            }
+        ]       
+        
+    },
+    {
+        path: '**',
+        redirectTo: 'dashboard'
+    }
+
+];
